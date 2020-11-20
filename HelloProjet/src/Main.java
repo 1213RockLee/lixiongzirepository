bca9519bd97ff56ca66f4db5ac07641e592d35b5
lxz <1005017724@qq.com>
2020-11-20 15:45:34 +0800

    好了

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -7,6 +7,7 @@ public class Main {
         System.out.println("HelloWorld!");
         System.out.println("HelloWorld2!");
         System.out.println("iamfive!");
+        System.out.println("iamfive第二次!");
 		
 		
     }
